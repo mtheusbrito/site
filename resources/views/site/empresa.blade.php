39cdd0ac30c434ed11209e70d849731ff3271587
mtheus <matheusbritodasilva@gmail.com>
2019-05-29 22:08:10 -0300

    projetos

--- resources/views/site/empresa.blade.php
+++ resources/views/site/empresa.blade.php
@@ -66,76 +66,76 @@
 
     <div class="row featurette">
         <div class="col-md-4">
-                <h3>Infraestrutura voltada para o crescimento do seu negócio</h3>
+            <h3>Infraestrutura voltada para o crescimento do seu negócio</h3>
         </div>
         <div class="col-md-8">
-                <p>Temos uma infraestrutura moderna, com servidores e computadores de última geração e links redundantes e os mais modernos frameworks para desenvolvimento de sistemas de forma que possamos atendê-lo melhor, mais rápido e com a máxima qualidade. Atendemos a empresas de todo o Brasil com agilidade e resolutividade.</p>
-        
+            <p>Temos uma infraestrutura moderna, com servidores e computadores de última geração e links redundantes e os mais modernos frameworks para desenvolvimento de sistemas de forma que possamos atendê-lo melhor, mais rápido e com a máxima qualidade. Atendemos a empresas de todo o Brasil com agilidade e resolutividade.</p>
+
         </div>
     </div>
     <hr class="featurette-divider">
     <div class="row featurette">
-                <div class="col-md-7 order-md-1">
-                                <h2 class="featurette-heading">Vantagem para sua empresa</h2>
-                                <p class="lead">
-                                                Estamos sendo a solução cada vez mais procurada como estratégia para aumentar a produtividade da área de TI. Isso porque, com a terceirização deste serviço, a empresa pode manter o foco na gestão do negócio, evitando desviar seu esforço para atividades de desenvolvimento e gestão de projetos, que exigem constante capacitação e investimento técnico.
-                                                Te proporcionamos diversos benefícios, como a possibilidade de adequar e equilibrar a equipe para atender picos de trabalho através da contratação dos serviços de profissionais especializados, além da corresponsabilidade do gerenciamento de prazos, qualidade e disponibilidade de recursos.
-                                </p>
-                </div>
-                <div class="col-md-5 order-md-2">
-                                <img class="featurette-image img-fluid mx-auto" src="/img/500x500.jpg" alt="Imagem">
-                </div>
+        <div class="col-md-7 order-md-1">
+            <h2 class="featurette-heading">Vantagem para sua empresa</h2>
+            <p class="lead">
+                Estamos sendo a solução cada vez mais procurada como estratégia para aumentar a produtividade da área de TI. Isso porque, com a terceirização deste serviço, a empresa pode manter o foco na gestão do negócio, evitando desviar seu esforço para atividades de desenvolvimento e gestão de projetos, que exigem constante capacitação e investimento técnico. Te proporcionamos diversos benefícios, como a possibilidade de adequar e equilibrar a equipe para atender picos de trabalho através da contratação dos serviços de profissionais especializados, além da corresponsabilidade do gerenciamento de prazos, qualidade e disponibilidade de recursos.
+            </p>
+        </div>
+        <div class="col-md-5 order-md-2">
+            <img class="featurette-image img-fluid mx-auto" src="/img/500x500.jpg" alt="Imagem">
+        </div>
 
     </div>
     <hr class="featurette-divider">
 
     <div class="row featurette">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
-                        <h2>Missão</h2>
-                        <p>Ser reconhecida como a empresa de melhor valor agregado em soluções na área de TI que atendam e excedam as expectativas de nossos clientes, com qualidade e tecnologia atualizada, prezando pelo meio-ambiente e com comprometimento e qualificação técnica de nossos colaboradores.</p>
-        
-                    </div>
-                    <div class="col-lg-4">
-                        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
-                        <h2>Visão</h2>
-                        <p>Ser uma empresa diferenciada e inovadora, com alta qualidade e que constantemente busca superar as expectativas dos clientes. Contarmos com colaboradores satisfeitos, que façam da parceria uma maneira efetiva de fazer negócios, em que a inovação é um lugar comum e a tecnologia está sempre a serviço do cliente.</p>
-                    </div>
-                    <div class="col-lg-4">
-                        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
-                        <h2>Valores</h2>
-                        <p>Ética <br/>
-                        Excelência<br/>
-                        Inovação<br/>
-                        Integridade<br/>
-                        Foco no cliente<br/>
-                        Comprometimento</p>
-                    </div>
-                </div>
-        
+        <div class="row">
+            <div class="col-lg-4">
+                <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
+                <h2>Missão</h2>
+                <p>Ser reconhecida como a empresa de melhor valor agregado em soluções na área de TI que atendam e excedam as expectativas de nossos clientes, com qualidade e tecnologia atualizada, prezando pelo meio-ambiente e com comprometimento e qualificação técnica de nossos colaboradores.</p>
+
             </div>
-            <hr class="featurette-divider">
-            <h2 class="featurette-heading" style="text-align: center">Nossa equipe</h2>
-            <div class="row featurette">
-                        <div class="col-lg-4">
-                                        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
-                                        <h3>Matheus Brito</h3>
-                                        <p>Desenvolvedor web</p>
-                                        <a href="mailto:mtheus.dev@gmail.com"><i class="far fa-envelope"></i></a>
-                                        
-                        </div>
-                        <div class="col-lg-4">
-                                        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
-                                        <h3>Pedro Henrrique Poiani</h3>
-                                        <p>Desenvolvedor web</p>
-                                        <a href="mailto:pedropoiani@gmail.com"><i class="far fa-envelope"></i></a>
-                        </div>
-                        <div class="col-lg-4">
-                        </div>
-                        <div class="col-lg-4">
-                        </div>
+            <div class="col-lg-4">
+                <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
+                <h2>Visão</h2>
+                <p>Ser uma empresa diferenciada e inovadora, com alta qualidade e que constantemente busca superar as expectativas dos clientes. Contarmos com colaboradores satisfeitos, que façam da parceria uma maneira efetiva de fazer negócios, em que a inovação é um lugar comum e a tecnologia está sempre a serviço do cliente.</p>
             </div>
+            <div class="col-lg-4">
+                <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
+                <h2>Valores</h2>
+                <p>Ética
+                    <br/> Excelência
+                    <br/> Inovação
+                    <br/> Integridade
+                    <br/> Foco no cliente
+                    <br/> Comprometimento
+                </p>
+            </div>
+        </div>
+
+    </div>
+    <hr class="featurette-divider">
+    <h2 class="featurette-heading" style="text-align: center">Nossa equipe</h2>
+    <div class="row featurette">
+        <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
+            <h3>Matheus Brito</h3>
+            <p>Desenvolvedor web</p>
+            <a href="mailto:mtheus.dev@gmail.com"><i class="far fa-envelope"></i></a>
+
+        </div>
+        <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
+            <h3>Pedro Henrrique Poiani</h3>
+            <p>Desenvolvedor web</p>
+            <a href="mailto:pedropoiani@gmail.com"><i class="far fa-envelope"></i></a>
+        </div>
+        <div class="col-lg-4">
+        </div>
+        <div class="col-lg-4">
+        </div>
+    </div>
 
 </div>
 
--- resources/views/site/projetos.blade.php
+++ resources/views/site/projetos.blade.php
@@ -1,7 +1,72 @@
-@extends('site_template.base')
-@section('conteudo')
-<div class="container">
-        <h1>Projetos</h1>
+@extends('site_template.base') @section('conteudo')
+<div class="container marketing">
+    <hr class="featurette-divider">
+    <div class="row featurette">
+        <div class="row">
+            <div class="col-lg-3">
+                <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
+                <h2>Levantamento dos requisitos</h2>
+                <p>Fazemos todo o levantamento e análise inicial dos requisitos do sistema.</p>
 
+            </div>
+            <div class="col-lg-3">
+                <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
+                <h2>Modelagem do sistema</h2>
+                <p>Modelamos o banco de dados, fazemos o caso de uso e desenvolvemos o sistema de acordo com a documentação levantada.</p>
+            </div>
+            <div class="col-lg-3">
+                <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
+                <h2>Desenvolvimento da aplicação</h2>
+                <p>Identificamos a melhor tecnologia para o desenvolvimento da aplicação e a equipe de análise e programação desenvolve todos os módulos definidos para o sistema.
+                </p>
+            </div>
+            <div class="col-lg-3">
+                <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100" height="100">
+                <h2>Documentação</h2>
+                <p>Documentação técnica e de utilização do sistema desenvolvido de forma a dar total condições de manutenção à equipe do cliente assim como, facilitar a aprendizado or parte do usuário.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <hr class="featurette-divider">
+    <div class="row featurette">
+        <div class="col-md-8 order-md-1">
+            <h2 class="featurette-heading">Projeto 1</h2>
+            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+            <p class="lead"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        </div>
+        <div class="col-md-4 order-md-2">
+            <img class="featurette-image img-fluid mx-auto" src="/img/500x500.jpg" alt="Imagem">
+        </div>
+
+    </div>
+    <hr class="featurette-divider">
+    <div class="row featurette">
+        <div class="col-md-8 order-md-2">
+            <h2 class="featurette-heading">Projeto 2</h2>
+            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+            <p class="lead">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+        </div>
+        <div class="col-md-4 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" src="/img/500x500.jpg" alt="Imagem">
+        </div>
+
+    </div>
+    <hr class="featurette-divider">
+    <div class="row featurette">
+        <div class="col-md-8 order-md-1">
+            <h2 class="featurette-heading">Projeto 3</h2>
+            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+            <p class="lead">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+        </div>
+        <div class="col-md-4 order-md-2">
+            <img class="featurette-image img-fluid mx-auto" src="/img/500x500.jpg" alt="Imagem">
+        </div>
+
+    </div>
+    <hr class="featurette-divider">
 </div>
 @endsection
